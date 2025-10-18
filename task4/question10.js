bac3c574031e0d2d33751556be9f1637b3389035
Athulya175 <athulyaa175@gmail.com>
2025-10-18 22:25:03 +0530

    set-2

--- task4/question10.js
+++ task4/question10.js
@@ -1,11 +1,12 @@
 function emailValidation(mail){
-    if(mail.includes(".com") && mail.includes("@")){
+    let extn=mail.slice(-4)
+    if(extn==".com" && mail.includes("@")){
         return true
     }
     else{
         return false
     }
 }
-console.log(emailValidation("test@example.com"))
+console.log(emailValidation("test.exa.@commple.com"))
 console.log(emailValidation("invalid.email.com"))
 console.log(emailValidation("user@domain"))
